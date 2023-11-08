57b7aae98a8471232f0e6eb9aaa1bbb7f6141cca
Dimitri Missoh <dimitri.missoh@maibornwolff.de>
2023-11-08 09:53:09 +0100

    initial commit

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Done: {count} time";
+        else
+            CounterBtn.Text = $"Done: {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
